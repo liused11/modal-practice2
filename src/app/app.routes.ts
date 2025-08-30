9745982ad3ce30c09b1800a1846edc9d234ba591
meowsocutee <kan21244@gmail.com>
2025-08-30 13:45:14 +0700

    update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'modal-example',
+    loadComponent: () => import('./modal-example/modal-example.page').then((m) => m.HomePage),
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'modal-example',
     pathMatch: 'full',
-  },  {
-    path: 'my-page',
-    loadComponent: () => import('./my-page/my-page.page').then( m => m.MyPagePage)
-  },
-  {
-    path: 'modal-example',
-    loadComponent: () => import('./modal-example/modal-example.page').then( m => m.ModalExamplePage)
-  },
-
+  }
 ];
