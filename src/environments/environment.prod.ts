72b4b68a057745ee9f74b1900f5c19ebcbdc9b7c
liused11 <burinrachakitja@gmail.com>
2025-09-21 23:37:25 +0700

    fix prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+// src/environments/environment.ts
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://pphvhjaaekecvllzfjrh.supabase.co/',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwaHZoamFhZWtlY3ZsbHpmanJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0NDUzMTUsImV4cCI6MjA3NDAyMTMxNX0.ZsT4FxyZaMMQc2x--IM2OiWQki6YB3_uFAPFYsrogJ0'
 };
